e1c33b5affda86fe476d2159423ebe64fab94d0b
Pascal-Owilly <pascalouma54@gmail.com>
2023-06-19 03:17:56 +0300

    p

--- enceptics/settings.py
+++ enceptics/settings.py
@@ -146,8 +146,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enceptics',
+        'USER': 'owilly',
+        'PASSWORD': 'owilly1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
